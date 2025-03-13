a655cfaac44ea8cc8bd2127f735467849909bd2a
petrvaneek <99393183+petrvaneek@users.noreply.github.com>
2025-03-13 11:01:29 +0100

    Update ce_gen.vhd

--- cv4_final_stopwatch/SOURCES/ce_gen.vhd
+++ cv4_final_stopwatch/SOURCES/ce_gen.vhd
@@ -6,41 +6,41 @@ use ieee.math_real.all;
 ----------------------------------------------------------------------------------
 ENTITY ce_gen IS
   GENERIC (
-    G_DIV_FACT          : POSITIVE := 500000
+    G_DIV_FACT : POSITIVE := 500000
   );
   PORT (
-    CLK                 : IN  STD_LOGIC;
-    SRST                : IN  STD_LOGIC;
-    CE                  : IN  STD_LOGIC;
-    CE_O                : OUT STD_LOGIC 
+    CLK   : IN  STD_LOGIC;
+    SRST  : IN  STD_LOGIC;
+    CE    : IN  STD_LOGIC;
+    CE_O  : OUT STD_LOGIC
   );
 END ENTITY ce_gen;
 ----------------------------------------------------------------------------------
 ARCHITECTURE Behavioral OF ce_gen IS
 ----------------------------------------------------------------------------------
-CONSTANT CNT_WIDTH : positive := positive(ceil(log2(real(G_DIV_FACT))));
-SIGNAL cnt: UNSIGNED (CNT_WIDTH-1 DOWNTO 0) := (OTHERS => '1');
-SIGNAL clk_en : STD_LOGIC := '0';  -- Clock enable signal
-SIGNAL cnt_div : UNSIGNED(3 downto 0) := (OTHERS => '0');
+CONSTANT CNT_WIDTH : POSITIVE := POSITIVE(ceil(log2(real(G_DIV_FACT))));
+SIGNAL cnt        : UNSIGNED (CNT_WIDTH-1 DOWNTO 0) := (OTHERS => '0');
+SIGNAL clk_en     : STD_LOGIC := '0';  
 ---------------------------------------------------------------------------------
 BEGIN
 ----------------------------------------------------------------------------------
-clk_en_gen : PROCESS (CLK) BEGIN
-	IF rising_edge(CLK) THEN
-		IF cnt_div = X"4" THEN
-			cnt_div <= X"0";
-			clk_en <= '1';
-		ELSE
-			cnt_div <= cnt_div + 1;
-			clk_en <= '0';
-		END IF;
-	END IF;
+clk_en_gen : PROCESS (CLK)
+BEGIN
+  IF rising_edge(CLK) THEN
+    IF SRST = '1' THEN
+      cnt    <= (OTHERS => '0'); 
+      clk_en <= '0';
+    ELSIF cnt = G_DIV_FACT-1 THEN
+      cnt    <= (OTHERS => '0');
+      clk_en <= '1';
+    ELSE
+      cnt    <= cnt + 1;
+      clk_en <= '0';
+    END IF;
+  END IF;
 END PROCESS clk_en_gen;
 
- CE_O <= clk_en;
-
-
-
+CE_O <= clk_en;
 ----------------------------------------------------------------------------------
 END ARCHITECTURE Behavioral;
 ----------------------------------------------------------------------------------
